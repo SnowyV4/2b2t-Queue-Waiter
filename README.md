32fc07d3bc5a1825f74484640dc0dcb65644ac2a
snowy <97677273+SnowyV4@users.noreply.github.com>
2024-01-13 23:46:27 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Queue Waiter Revamp
+
+- Wait out server queues while you are away. This variant includes web hook support!
+
+# Authors
+- Original author: OBNinjaa
+- This addon: SnowyV4
+
+# Install
+- First add all the required info into the "config.json" file. Username: mc username(for mineflayer) webhookUrl: (input ur webhook url)
+
+```bash
+npm i
+node .
+```
+
+# Authentication
+
+- The tool uses Microsoft Authentication (MSA) for a secure login process. This means you won't have to provide your email and password.
+- You may be asked to visit [microsoft.com](https://microsoft.com/link) and enter the **authentication** code provided in the console in order to authenticate.
+
+- ## 🔗 Links
+- original creator(OBNinjaa) discord links below:
+
+[![](https://dcbadge.vercel.app/api/server/NysD9gyx7R)](https://discord.gg/NysD9gyx7R)
---
+++ src/proxy.js
@@ -0,0 +1,126 @@
+//originally made by OBNinja, recoded by SnowyV4
+const { createClient, createServer, states } = require("minecraft-protocol");
+const { Client, GatewayIntentBits, EmbedBuilder, WebhookClient, Attachment, parseWebhookURL, Webhook } = require('discord.js')
+const { MessageContent, GuildMessages, Guilds, GuildMembers } = GatewayIntentBits
+
+let userClient;
+const packets = [];
+let previousQueueNumber = null;
+
+const WEBHOOK = require("./config.json")
+const { config } = require("./config.json");
+
+const proxyClient = createClient({
+  username: config.username,
+  auth: "microsoft",
+  host: "2B2T.ORG",
+  port: 25565,
+  keepAlive: true,
+  version: "1.19.4",
+  hideErrors: true,
+});
+
+proxyClient.on("packet", (data, meta) => {
+  if (meta.name === "keep_alive") return;
+  if (!["keep_alive", "success", "custom_payload", "encryption_begin", "compress", "look", "flying", "open_window", "close_window", "close_window", "player_chat", "profileless_chat"].includes(meta.name)) packets.push([meta, data]);
+
+  if (meta.name === "set_title_subtitle") {
+    try {
+      const parsedData = JSON.parse(data.text);
+      const queueText = parsedData.text;
+      const queueNumber = parseInt(queueText.match(/\d+/)[0]);
+      if (previousQueueNumber !== queueNumber) {
+        const currentTime = new Date().toLocaleTimeString(); // Get the current time
+        console.log(`[${currentTime}] Position in queue: ${queueNumber}`); // Display time with queue position
+        WEBHOOK.send({
+          content: `[${currentTime}] Position in queue: ${queueNumber}`,
+          username: "2b2t proxy",
+          avatarURL: "https://pbs.twimg.com/media/EjVehVWWoAMd5QK?format=jpg&name=900x900"
+        })
+        previousQueueNumber = queueNumber;
+      }
+    } catch (err) {
+      return;
+    }
+  }
+
+  if (!userClient || meta.state !== states.PLAY || userClient.state !== states.PLAY) return;
+  userClient.write(meta.name, data);
+  if (meta.name === "set_compression") userClient.compressionThreshold = data.threshold;
+});
+
+proxyClient.on("raw", (buffer, meta) => {
+  if (!userClient || meta.name === "keep_alive" || meta.state !== states.PLAY || userClient.state !== states.PLAY) return;
+});
+
+proxyClient.on("end", () => {
+  if (!userClient) return;
+  userClient.end("Proxy client ended");
+});
+
+proxyClient.on("error", (error) => {
+  if (!userClient) return;
+  console.error("Proxy client error:", error);
+  WEBHOOK.send({
+    content: `[${currentTime}] Proxy error received: ${error}`,
+    username: "2b2t proxy",
+    avatarURL: "https://pbs.twimg.com/media/EjVehVWWoAMd5QK?format=jpg&name=900x900"
+  })
+  userClient.end(error);
+});
+
+const proxyServer = createServer({
+  "online-mode": true,
+  host: "0.0.0.0",
+  port: 25566,
+  keepAlive: false,
+  version: "1.19.4",
+});
+
+proxyServer.on("login", (client) => {
+  console.log(`${client.username} has connected`);
+  WEBHOOK.send({
+    content: `[${currentTime}] ${client.username} has connected!`,
+    username: "2b2t proxy",
+    avatarURL: "https://pbs.twimg.com/media/EjVehVWWoAMd5QK?format=jpg&name=900x900"
+  })
+
+  packets.forEach((p) => {
+    const meta = p[0];
+    const data = p[1];
+    client.write(meta.name, data);
+  });
+
+  userClient = client;
+
+  client.on("packet", (data, meta) => {
+    if (meta.name === "keep_alive") return;
+    if (!proxyClient || meta.state !== states.PLAY || proxyClient.state !== states.PLAY) return;
+    proxyClient.write(meta.name, data);
+  });
+
+  client.on("raw", (buffer, meta) => {
+    if (meta.name === "keep_alive") return;
+    if (!proxyClient || meta.state !== states.PLAY || proxyClient.state !== states.PLAY) return;
+  });
+
+  client.on("end", () => {
+    if (!proxyClient) return;
+    console.log(`${client.username} has disconnected`);
+    WEBHOOK.send({
+      content: `[${currentTime}] ${client.username} has disconnected!`,
+      username: "2b2t proxy",
+      avatarURL: "https://pbs.twimg.com/media/EjVehVWWoAMd5QK?format=jpg&name=900x900"
+    })
+  });
+
+  client.on("error", (error) => {
+    if (!proxyClient) return;
+    console.error(error);
+    WEBHOOK.send({
+      content: `[${currentTime}] Error received: ${error}`,
+      username: "2b2t proxy",
+      avatarURL: "https://pbs.twimg.com/media/EjVehVWWoAMd5QK?format=jpg&name=900x900"
+    })
+  });
+});
